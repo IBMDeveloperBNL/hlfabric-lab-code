550a9cc3ea52591e684f24664b9e6133b747e854
Edward Ciggaar <edward_ciggaar@nl.ibm.com>
2021-03-15 18:11:46 +0100

    Updates and migration to HLFv2.2

--- business-service/src/blockchainClient.ts
+++ business-service/src/blockchainClient.ts
@@ -1,80 +1,93 @@
-const { FileSystemWallet, Gateway } = require('fabric-network');
-const fs = require('fs');
-const path = require('path');
+import {BlockEvent, Contract, ContractEvent, ContractListener, DefaultCheckpointers, Gateway, ListenerOptions, Network, TransactionEvent, Wallet, Wallets} from 'fabric-network';
+import * as fs from 'fs';
+import * as path from 'path';
 
 const configPath = path.join(process.cwd(), './local_fabric/config.json');
 const configJSON = fs.readFileSync(configPath, 'utf8');
 const config = JSON.parse(configJSON);
 
-let appAdmin = config.appAdmin;
+let appAdmin: string = config.appAdmin;
 let gatewayDiscovery = config.gatewayDiscovery;
-let connection_file = config.connection_file;
+let connection_file: string = config.connection_file;
 
 // connect to the connection file
 const ccpPath = path.join(process.cwd(), './local_fabric/' + connection_file);
 const ccpJSON = fs.readFileSync(ccpPath, 'utf8');
 const ccp = JSON.parse(ccpJSON);
 
-// A wallet stores a collection of identities for use
-const wallet = new FileSystemWallet('./local_fabric/wallet');
-
 // Gateway to interact with the blockchain network
-const gateway = new Gateway();
+const gateway: Gateway = new Gateway();
+
+// Channel and contract name
+const CHANNEL = 'mychannel';
+const CONTRACT = 'global-citizen';
 
 export module BlockChainModule {
 
   export class BlockchainClient {
     async connectToNetwork() {
-
       try {
-        await gateway.connect(ccp, { wallet, identity: appAdmin, discovery: gatewayDiscovery });
+        // A wallet stores a collection of identities for use
+        const wallet: Wallet = await Wallets.newFileSystemWallet('./local_fabric/wallet');
+
+        await gateway.connect(ccp, {wallet, identity: appAdmin, discovery: gatewayDiscovery});
 
         // Connect to our local fabric
-        const network = await gateway.getNetwork('mychannel');
+        const network: Network = await gateway.getNetwork('mychannel');
 
-        console.log('Connected to mychannel. ');
+        console.log('**********************************************************************************************************');
+        console.log('--> connecting to channel: ' + CHANNEL);
 
         // Get the contract we have installed on the peer
-        const contract = await network.getContract('global-citizen');
+        const contract: Contract = network.getContract(CONTRACT);
 
-        await contract.addContractListener('my-contract-listener', 'PledgeEvent', (err:any, event:any, blockNumber:any, transactionId:any, status:any) => {
-          if (err) {
-            console.error(err);
-            return;
-          }
-    
-          // Convert event to something we can parse 
-          event = event.payload.toString();
-          event = JSON.parse(event);
-    
-          // Output the PledgeEvent
-          console.log('***************************************************************** Start Pledge Event *****************************************************************');
-          console.log(`type: ${event.type}`);
-          console.log(`pledgeId: ${event.pledgeId}`);
-          console.log(`name: ${event.name}`);
-          console.log(`description: ${event.description}`);
-          console.log(`fundsRequired: ${event.fundsRequired}`);
-          
-          if (event.funds) {
-            console.log(`   fundingType: ${event.funds.fundingType}`);
-            console.log(`   approvedFunding: ${event.funds.approvedFunding}`);
-            console.log(`   totalFundsReceived: ${event.funds.totalFundsReceived}`);
-            console.log(`   fundsPerInstallment: ${event.funds.fundsPerInstallment}`);
-          }
-          
-          console.log(`status: ${event.status}`); 
-          console.log(``);
-          console.log(`Block Number: ${blockNumber} Transaction ID: ${transactionId} Status: ${status}`);
-          console.log('***************************************************************** End Pledge Event *******************************************************************');
-        });
+        const listener: ContractListener = async (event: ContractEvent) => {
+          if (event.eventName === 'PledgeEvent') {
+            if (event.payload) {
+              const eventPayload = JSON.parse(event.payload.toString('utf8'));
+              const eventTransaction: TransactionEvent = event.getTransactionEvent();
+              const eventBlock: BlockEvent = eventTransaction.getBlockEvent();
 
-        let networkObj = {
+              // Output the PledgeEvent
+              console.log('\n--> ******** received pledge event ********');
+              console.log(`-->    type: ${eventPayload.type}`);
+              console.log(`-->    pledgeId: ${eventPayload.pledgeId}`);
+              console.log(`-->    name: ${eventPayload.name}`);
+              console.log(`-->    description: ${eventPayload.description}`);
+              console.log(`-->    fundsRequired: ${eventPayload.fundsRequired}`);
+
+              if (eventPayload.funds) {
+                console.log(`-->       fundingType: ${eventPayload.funds.fundingType}`);
+                console.log(`-->       approvedFunding: ${eventPayload.funds.approvedFunding}`);
+                console.log(`-->       totalFundsReceived: ${eventPayload.funds.totalFundsReceived}`);
+                console.log(`-->       fundsPerInstallment: ${eventPayload.funds.fundsPerInstallment}`);
+              }
+
+              console.log(`-->    pledge status: ${eventPayload.status}`);
+              console.log(`--> `);
+              console.log(`-->    block number: ${eventBlock.blockNumber}`);
+              console.log(`-->    transaction ID: ${eventTransaction.transactionId}`);
+              console.log(`-->    transaction status: ${eventTransaction.status}`);
+              console.log('--> ******** processed pledge event ********\n');
+            }
+          }
+        };
+
+        // Define file based checkpointer to keep track of last block and transactions in that block, that have been seen by this client.
+        const options: ListenerOptions = {
+          checkpointer: await DefaultCheckpointers.file('./checkpoint.json')
+        }
+
+        // Add the listener defined above to the contract so that it can receive smart contract events emitted by the chaincode
+        await contract.addContractListener(listener, options);
+
+        // Define network object that contains both the contract and the network object
+        const networkObj = {
           contract: contract,
           network: network
         };
 
         return networkObj;
-
       } catch (error) {
         console.log(`Error processing transaction. ${error}`);
         console.log(error.stack);
@@ -83,52 +96,60 @@ export module BlockChainModule {
       }
     }
 
-    async disconnectFromNetwork() {
+    disconnectFromNetwork() {
       // Properly disconnecting from the network when finished
-      let response = await gateway.disconnect();
-      return response;
+      console.log('--> disconnecting from channel: ' + CHANNEL);
+      console.log('**********************************************************************************************************');
+      console.log(' ');
+
+      return gateway.disconnect();
     }
 
     async createProjectPledge(args: any) {
-      // Calling transaction1 transaction in the smart contract
-      let response = await args.contract.submitTransaction(args.function, args.aidOrg, args.pledgeNumber, args.name, args.desc, args.fundsRequired);
-
-      return response;
+      // Calling createProjectPledge transaction in the smart contract
+      console.log('--> invoking createProjectPledge(ctx, "' + args.aidOrg + '", "' + args.pledgeNumber + '", "' + args.name + '", "' + args.desc + '", "' + args.fundsRequired + '")');
+      return await args.contract.submitTransaction(args.function, args.aidOrg, args.pledgeNumber, args.name, args.desc, args.fundsRequired);
     }
 
     async sendPledgeToGlobalCitizen(args: any) {
-      // Calling transaction1 transaction in the smart contract
-      let response = await args.contract.submitTransaction(args.function, args.pledgeId);
-
-      return response;
+      // Calling sendPledgeToGlobalCitizen transaction in the smart contract
+      console.log('--> invoking sendPledgeToGlobalCitizen(ctx, "' + args.pledgeId + '")');
+      return await args.contract.submitTransaction(args.function, args.pledgeId);
     }
 
     async sendPledgeToGovOrg(args: any) {
-      // Calling transaction1 transaction in the smart contract
-      let response = await args.contract.submitTransaction(args.function, args.pledgeId);
-
-      return response;
+      // Calling sendPledgeToGovOrg transaction in the smart contract
+      console.log('--> invoking sendPledgeToGovOrg(ctx, "' + args.pledgeId + '")');
+      return await args.contract.submitTransaction(args.function, args.pledgeId);
     }
 
     async updatePledge(args: any) {
-      // Calling transaction1 transaction in the smart contract
-      let response = await args.contract.submitTransaction(args.function, args.pledgeId, args.fundingType, args.approvedFunding, args.fundsPerInstallment);
-
-      return response;
+      // Calling updatePledge transaction in the smart contract
+      console.log('--> invoking updatePledge(ctx, "' + args.pledgeId + '", "' + args.fundingType + '", "' + args.approvedFunding + '", "' + args.fundsPerInstallment + '")');
+      return await args.contract.submitTransaction(args.function, args.pledgeId, args.fundingType, args.approvedFunding, args.fundsPerInstallment);
     }
 
     async transferFunds(args: any) {
-      // Calling transaction1 transaction in the smart contract
-      let response = await args.contract.submitTransaction(args.function, args.pledgeId);
-
-      return response;
+      // Calling transferFunds transaction in the smart contract
+      console.log('--> invoking transferFunds(ctx, "' + args.pledgeId + '")');
+      return await args.contract.submitTransaction(args.function, args.pledgeId);
     }
 
     async queryProject(args: any) {
-      // Calling transaction1 transaction in the smart contract
-      let response = await args.contract.evaluateTransaction(args.function, args.id);
-      console.log(JSON.parse(response));
-      return JSON.parse(response);
+      try {
+        // Calling readProjectPledge transaction in the smart contract
+        console.log('--> invoking readProjectPledge(ctx, "' + args.id + '")');
+        let response = await args.contract.evaluateTransaction(args.function, args.id);
+        console.log('--> result:\n');
+        console.log(JSON.parse(response));
+        console.log('');
+
+        return JSON.parse(response);
+      } catch (error) {
+        console.log(error.message);
+
+        return error;
+      }
     }
   }
 }
